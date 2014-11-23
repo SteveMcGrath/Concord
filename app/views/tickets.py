1f838c0643789990af14a5c4e4c0178d72263303
Steven McGrath <steve@chigeek.com>
2014-11-23 14:50:35 -0600

    Added ticket payment processing.

--- app/views/tickets.py
+++ app/views/tickets.py
@@ -1,25 +1,19 @@
-from flask import render_template, flash, redirect, session, url_for, abort, g
+from flask import render_template, flash, redirect, session, url_for, abort, g, request
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, login_manager, forms
-from app.models import User, Ticket
+from app.models import User, Ticket, Purchase, DiscountCode
 from sqlalchemy import desc
-from datetime import datetime
+from datetime import date
+import stripe
+import smtplib
 
+stripe.api_key = app.config['STRIPE_SKEY']
 
 @app.route('/tickets')
 def tickets():
     #attendees = Ticket.query.filter_by(ticket_type='attendee').count()
     #tickets = 'On Sale %s' % app.config['TICKETS_START'].strftime('%Y-%m-%d %H:%M')
     return render_template('ticketing/main.html', title='Tickets')
-
-
-@app.route('/tickets/print/<ticket_id>')
-def ticket_print(ticket_id):
-    '''
-    Generates a printable ticket.
-    '''
-    ticket = Ticket.query.filter_by(ticket_hash=ticket_id).first_or_404()
-    return ticket.generate(app.config['CONFERENCE_EVENT'])
 
 
 @app.route('/tickets/press', methods=['GET', 'POST'])
@@ -32,3 +26,137 @@ def press_ticket():
     #    press = 
     # Stub all of that out until after vacation...
     return render_template('ticketing/press.html', title='Press Tickets')
+
+
+@app.route('/tickets/purchase', methods=['GET', 'POST'])
+def purchase_tickets():
+    tickets = app.config['TICKETS']
+    form = forms.PurchaseForm()
+    if form.validate_on_submit():
+        purchase = Purchase()
+        form.populate_obj(purchase)
+
+        # First thing that we need to do is check to make sure that the email
+        # actually is a .edu address.  If not, tell the user they fucked up and
+        # set it back to attendee. 
+        if purchase.ticket_type == 'student' and purchase.email[-3:] != 'edu':
+            flash('Not a student, defaulting to Attendee')
+            # The student was a lie!  Reset it to attendee.
+            purchase.ticket_type = 'attendee'
+            purchase.price = tickets[purchase.ticket_type]['price']
+        
+        # Next we need to match up the ticket types to make sure that no one
+        # was mucking about with the ticket types.  If everything matches, then
+        # go ahead and trust what the form is telling us and apply the price.
+        if (purchase.ticket_type in tickets 
+            and tickets[purchase.ticket_type]['visible']
+            and (tickets[purchase.ticket_type]['expiration'] is None
+                or date.today() < tickets[purchase.ticket_type]['expiration'])):
+            purchase.price = tickets[purchase.ticket_type]['price']
+        else:
+            flash('Invalid Ticket Type...resetting to Attendee')
+            # If it looks like someone was actually sending odd form values,
+            # then we wont trust the ticket type and override it with a regular
+            # attendee ticket type.
+            purchase.ticket_type = 'attendee'
+            purchase.price = tickets[purchase.ticket_type]['price']
+        if form.discountcode.data != '':
+            # Lets check to see if the discount code is valid.  If it is, then
+            # we will be overriding the price and the ticket type associated
+            # with this purchase.
+            code = DiscountCode.query.filter_by(code=form.discountcode.data).first()
+            if code is None and code.uses < 1:
+                flash('Invalid or Expired Discount Code')
+            else:
+                purchase.price = code.price
+                purchase.ticket_type = code.t_type
+
+        # Add the purchase to the database and then return the confirmation page.
+        db.session.add(purchase)
+        db.session.commit()
+        return render_template('ticketing/confirm.html', 
+            purchase=purchase, 
+            title='Purchase Confirmation',
+            stripe_key=app.config['STRIPE_PKEY'])
+    return render_template('ticketing/purchase.html', 
+            form=form,
+            title='Ticket Purchasing')
+
+
+@app.route('/tickets/charge/<purchase_hash>', methods=['POST'])
+def charge_card(purchase_hash):
+    purchase = Purchase.query.filter_by(ref_hash=purchase_hash).first_or_404()
+
+    if purchase.price > 0:
+        # Now we will actually charge the card.  Based off of how the stripe
+        # examples looked, this seems to be the correct way to charge a card.
+        purchase.payment_type = 'stripe'
+        purchase.payment_token = request.form['stripeToken']
+        customer = stripe.Customer.create(
+            email=purchase.email, 
+            card=purchase.payment_token
+        )
+
+        charge = stripe.Charge.create(
+            customer=customer.id,
+            amount=int(purchase.price * 100),  #Stripe Expects the payment in cents.
+            currency='usd',
+            description='CircleCityCon %s Ticket' % purchase.ticket_type
+        )
+    else:
+        purchase.payment_type = 'comp'
+        purchase.payment_token = 'comp'
+
+    # Now to update the payment object with the payment information, append
+    # the ticket codes to the purchase, and then to mark the payment as done.
+    if purchase.ticket_type == 'family':
+        purchase.tickets.append(Ticket(purchase.email, ticket_type='attendee'))
+        purchase.tickets.append(Ticket(purchase.email, ticket_type='attendee'))
+        purchase.tickets.append(Ticket(purchase.email, ticket_type='child'))
+        purchase.tickets.append(Ticket(purchase.email, ticket_type='child'))
+        purchase.tickets.append(Ticket(purchase.email, ticket_type='child'))
+    else:
+        purchase.tickets.append(Ticket(purchase.email, ticket_type=purchase.ticket_type))
+    purchase.completed = True
+
+    # If there is a discount code involved, we will need to decriment the use
+    # counter as well.
+    if purchase.discountcode != '':
+        code = DiscountCode.query.filter_by(code=purchase.discountcode).first()
+        if code is not None:
+            code.uses -= 1
+            db.session.merge(code)
+
+    # Commit all of the database changes!
+    db.session.merge(purchase)
+    db.session.commit()
+
+    # Next we will generate the email that will be sent to the address specified
+    # with the redemption code.
+    msg = render_template('ticketing/pickup.eml', 
+        purchase=purchase,
+        url=app.config['SITE_ADDRESS'])
+    print msg
+    #smtp = smtplib.SMTP('localhost')
+    #smtp.sendmail('no-reply@circlecitycon.com', [purchase.email], msg)
+
+    # Lastly tell the user that the purchase is complete and to check their
+    # email for the ticket codes.
+    return render_template('ticketing/charged.html', 
+        purchase=purchase,
+        title='Purchase Completed')
+
+
+@app.route('/tickets/pickup/<purchase_hash>')
+def ticket_pickup(purchase_hash):
+    purchase = Purchase.query.filter_by(ref_hash=purchase_hash, completed=True).first_or_404()
+    return render_template('ticketing/pickup.html', purchase=purchase)
+
+
+@app.route('/tickets/print/<ticket_id>')
+def ticket_print(ticket_id):
+    '''
+    Generates a printable ticket.
+    '''
+    ticket = Ticket.query.filter_by(ticket_hash=ticket_id).first_or_404()
+    return ticket.generate(app.config['CONFERENCE_EVENT'])

55d5775a5fadd38eddb55afd9fbe71df212352db
Steven McGrath <steve@chigeek.com>
2015-02-17 21:12:31 -0600

    The talk page is live! bwahahahaha!

--- site/app/views/__init__.py
+++ site/app/views/__init__.py
@@ -37,8 +37,7 @@ def training():
 
 @app.route('/talks')
 def talks():
-    #return render_template('schedule/talks.html', title='Talks List')
-    return render_template('construction.html', title='Talks List')
+    return render_template('schedule/talks.html', title='Talks List')
 
 
 @app.route('/sponsors')

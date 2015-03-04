a62df5666b980ccb1088c5c75ea76e396b42cb46
Steven McGrath <steve@chigeek.com>
2015-03-04 00:55:10 -0600

    reg edit

--- registration/app/models.py
+++ registration/app/models.py
@@ -12,11 +12,9 @@ class Ticket(db.Model):
     __tablename__ = 'tickets'
     id = db.Column(db.Integer, primary_key=True)
     ticket_type = db.Column(db.Text)
-    purchase_id = db.Column(db.Integer, db.ForeignKey('purchases.id'))
-    date = db.Column(db.DateTime, default=datetime.now())
     ticket_hash = db.Column(db.Text)
-    user_id = db.Column(db.Integer, default=None)
     redeemed = db.Column(db.Boolean, default=False)
-    marketing = db.Column(db.Boolean, default=True)
     email = db.Column(db.Text)
     name = db.Column(db.Text)
+
+

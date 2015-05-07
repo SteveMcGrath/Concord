8941b5d1ee384db95ef9fbf8b81c11514dd4fdbf
Steven McGrath <steve@chigeek.com>
2015-05-07 17:20:34 -0500

    Added classes into stats

--- site/app/views/statistics.py
+++ site/app/views/statistics.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, session, url_for, abort, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, login_manager, forms
-from app.models import Ticket, Purchase
+from app.models import Ticket, Purchase,Seats
 from datetime import datetime, timedelta
 from sqlalchemy import func
 
@@ -12,5 +12,6 @@ def stats_main_page():
     return render_template('stats/main.html',
         t_total=Ticket.query.filter().count(),
         t_new=Ticket.query.filter(Ticket.date >= seven_days_ago).count(),
+        classes=db.session.query(Seat.name, func.count(Seat.name)).group_by(Seat.name).all()
         t_types=db.session.query(Ticket.ticket_type, func.count(Ticket.ticket_type)).group_by(Ticket.ticket_type).all(),
     )
